a71479192935f0997fec953bc163b9694be712d8
Hamid Tavakoli <seyedhat@uci.edu>
2018-04-30 13:00:35 -0700

    Crating diff

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express()
 
 console.log("On branch test")
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/test', (req, res) => res.send('Hello World!'))
+console.log("creating diff!!")
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
