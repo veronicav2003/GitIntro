8ead4bce6ff31cbb6426b574920b8a390933dc6f
Hamid Tavakoli <seyedhat@uci.edu>
2018-04-30 12:57:16 -0700

    test branch
    trying to mefge

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+
+console.log("On branch test")
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/test', (req, res) => res.send('Hello World!'))
 
