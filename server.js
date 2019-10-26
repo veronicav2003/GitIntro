f1e98e3c170c2abfffdc28bdf74848a8f77a5160
Hamid Tavakoli <nick@seodapop.com>
2019-10-25 22:36:10 -0700

    another small change

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express()
 
 console.log("On branch test")
 app.get('/', (req, res) => res.send('Hello World!'))
-
 console.log("creating diff!!")
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
